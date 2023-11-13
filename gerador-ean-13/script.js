cddb50b1569a5e0699bf943a5a8cd23e17db9375
Paulo Devinky <120813278+PauloDevinky@users.noreply.github.com>
2023-11-13 15:16:29 -0300

    Update script.js

--- gerador-ean-13/script.js
+++ gerador-ean-13/script.js
@@ -11,16 +11,24 @@ function generateBarcodes() {
         return;
     }
 
-    for (var i = 0; i < inputQuantity; i++) {
-        // Gere o código EAN-13 usando JsBarcode
-        var barcodeValue = inputDigits + i;
-        
-        JsBarcode(barcodeSvg, barcodeValue, {
-            format: "EAN13",
-            displayValue: true
-        });
+    // Adicione a verificação se a biblioteca JsBarcode está definida
+    if (typeof JsBarcode !== 'undefined') {
+        for (var i = 0; i < inputQuantity; i++) {
+            // Gere o código EAN-13 usando JsBarcode
+            var barcodeValue = inputDigits + i;
 
-        // Adicione uma quebra de linha para códigos múltiplos
-        barcodeSvg.innerHTML += "<br>";
+            JsBarcode(barcodeSvg, barcodeValue, {
+                format: "EAN13",
+                displayValue: true
+            });
+
+            // Adicione uma quebra de linha para códigos múltiplos
+            barcodeSvg.innerHTML += "<br>";
+        }
+    } else {
+        alert("Erro: A biblioteca JsBarcode não está carregada corretamente.");
     }
 }
+
+// Adiciona um ouvinte de evento ao botão
+document.getElementById("generateButton").addEventListener("click", generateBarcodes);
