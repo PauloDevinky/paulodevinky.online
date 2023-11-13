fb73cd3d931f7319217f6bae437d7b9b3d338a0d
Paulo Devinky <120813278+PauloDevinky@users.noreply.github.com>
2023-11-13 16:41:44 -0300

    Update script.js

--- gerador-ean-13/script.js
+++ gerador-ean-13/script.js
@@ -19,54 +19,19 @@ function generateBarcodes() {
     var checkDigit = calculateEAN13CheckDigit(fullCode);
     var barcodeValue = fullCode + checkDigit;
 
-    JsBarcode(barcodeSvg, barcodeValue, {
-      format: "EAN13",
-      displayValue: true
-    });
+    // Adicionando console.log para verificar se JsBarcode está definida
+    console.log("JsBarcode:", JsBarcode);
 
-    barcodeSvg.innerHTML += "<br>";
+    // Certifique-se de que a variável JsBarcode seja uma função antes de chamá-la
+    if (typeof JsBarcode === 'function') {
+      JsBarcode(barcodeSvg, barcodeValue, {
+        format: "EAN13",
+        displayValue: true
+      });
+
+      barcodeSvg.innerHTML += "<br>";
+    } else {
+      alert("Erro: A biblioteca JsBarcode não está carregada corretamente.");
+    }
   }
-}
-
-function calculateEAN13CheckDigit(code) {
-  // Certifique-se de que o código fornecido tem 12 dígitos
-  if (code.length !== 12) {
-    throw new Error("O código deve ter exatamente 12 dígitos.");
-  }
-
-  // Converte o código em uma matriz de dígitos
-  var digits = code.split("").map(Number);
-
-  // Calcula a soma dos dígitos nas posições ímpares
-  var oddSum = digits
-    .filter(function (_, index) {
-      return index % 2 === 0;
-    })
-    .reduce(function (sum, digit) {
-      return sum + digit;
-    }, 0);
-
-  // Multiplica a soma por 3
-  oddSum *= 3;
-
-  // Calcula a soma dos dígitos nas posições pares
-  var evenSum = digits
-    .filter(function (_, index) {
-      return index % 2 !== 0;
-    })
-    .reduce(function (sum, digit) {
-      return sum + digit;
-    }, 0);
-
-  // Calcula a soma total
-  var totalSum = oddSum + evenSum;
-
-  // Encontra o próximo múltiplo de 10
-  var nextMultipleOfTen = Math.ceil(totalSum / 10) * 10;
-
-  // Calcula o dígito verificador
-  var checkDigit = nextMultipleOfTen - totalSum;
-
-  // Retorna o dígito verificador como string
-  return checkDigit.toString();
 }
