fcb8509ad677b3b5c565d33c777a727ff231e7bb
Paulo Devinky <120813278+PauloDevinky@users.noreply.github.com>
2023-11-13 14:30:04 -0300

    Update script.js

--- gerador-ean-13/script.js
+++ gerador-ean-13/script.js
@@ -1,37 +1,26 @@
-function loadJsBarcode(callback) {
-    var script = document.createElement('script');
-    script.type = 'text/javascript';
-    script.src = 'https://cdn.jsdelivr.net/npm/jsbarcode@3.11.0/dist/JsBarcode.all.min.js';
-    script.onload = callback;
-    document.head.appendChild(script);
-}
+function generateBarcodes() {
+    var inputDigits = document.getElementById("inputDigits").value;
+    var inputQuantity = document.getElementById("inputQuantity").value;
+    var barcodeSvg = document.getElementById("barcodeSvg");
 
-// Quando a biblioteca estiver carregada, chame a função principal
-loadJsBarcode(function() {
-    // Função principal que gera os códigos
-    function generateBarcodes() {
-        var inputDigits = document.getElementById("inputDigits").value;
-        var inputQuantity = document.getElementById("inputQuantity").value;
-        var barcodeSvg = document.getElementById("barcodeSvg");
+    // Limpar códigos anteriores
+    barcodeSvg.innerHTML = "";
 
-        // Limpar códigos anteriores
-        barcodeSvg.innerHTML = "";
-
-        if (!/^\d{5}$/.test(inputDigits)) {
-            alert("Digite 5 dígitos válidos do CPF ou CNPJ.");
-            return;
-        }
-
-        for (var i = 0; i < inputQuantity; i++) {
-            // Gere o código EAN-13 usando JsBarcode
-            var barcodeValue = inputDigits + i;
-            JsBarcode(barcodeSvg, barcodeValue, {
-                format: "EAN13",
-                displayValue: true,
-            });
-
-            // Adicione uma quebra de linha para códigos múltiplos
-            barcodeSvg.innerHTML += "<br>";
-        }
+    if (!/^\d{5}$/.test(inputDigits)) {
+        alert("Digite 5 dígitos válidos do CPF ou CNPJ.");
+        return;
     }
-});
+
+    for (var i = 0; i < inputQuantity; i++) {
+        // Gere o código EAN-13 usando JsBarcode
+        var barcodeValue = inputDigits + i;
+        
+        JsBarcode(barcodeSvg, barcodeValue, {
+            format: "EAN13",
+            displayValue: true
+        });
+
+        // Adicione uma quebra de linha para códigos múltiplos
+        barcodeSvg.innerHTML += "<br>";
+    }
+}
