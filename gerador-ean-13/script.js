fc04bdefbeaa344da16e02dfaa8fc27ce97fa8ff
Paulo Devinky <120813278+PauloDevinky@users.noreply.github.com>
2023-11-14 14:08:59 -0300

    Update script.js

--- gerador-ean-13/script.js
+++ gerador-ean-13/script.js
@@ -19,16 +19,11 @@ function generateBarcodes() {
   var lastGeneratedNumber = parseInt(localStorage.getItem("lastGeneratedNumber")) || 0;
 
   for (var i = 0; i < inputQuantity; i++) {
-    var baseCode = "789" + inputDigits;
-    var fullCode = baseCode + (lastGeneratedNumber + 1 + i).toString().padStart(4, '0');
-    var checkDigit = calculateEAN13CheckDigit(fullCode);
-    var barcodeValue = fullCode + checkDigit;
-
+    var barcodeValue = generateEAN13(inputDigits, lastGeneratedNumber + 1 + i);
     JsBarcode(barcodeContainer, barcodeValue, {
       format: "EAN13",
       displayValue: true
     });
-
     barcodeContainer.innerHTML += "<br>";
   }
 
@@ -38,6 +33,35 @@ function generateBarcodes() {
 
 // Função para calcular o dígito verificador EAN-13
 function calculateEAN13CheckDigit(code) {
-  // ... (código existente para calcular o dígito verificador)
+  if (code.length !== 12) {
+    throw new Error("O código deve ter exatamente 12 dígitos.");
+  }
+
+  var digits = code.split("").map(Number);
+
+  var oddSum = digits
+    .filter(function (_, index) {
+      return index % 2 === 0;
+    })
+    .reduce(function (sum, digit) {
+      return sum + digit;
+    }, 0);
+
+  oddSum *= 3;
+
+  var evenSum = digits
+    .filter(function (_, index) {
+      return index % 2 !== 0;
+    })
+    .reduce(function (sum, digit) {
+      return sum + digit;
+    }, 0);
+
+  var totalSum = oddSum + evenSum;
+
+  var nextMultipleOfTen = Math.ceil(totalSum / 10) * 10;
+
+  var checkDigit = nextMultipleOfTen - totalSum;
+
   return checkDigit.toString();
 }
