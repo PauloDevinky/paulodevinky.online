ffc10ca887a21d931d12a7b6117d10539a2ff684
Paulo Devinky <120813278+PauloDevinky@users.noreply.github.com>
2023-11-14 11:41:57 -0300

    Update script.js

--- gerador-ean-13/script.js
+++ gerador-ean-13/script.js
@@ -1,10 +1,14 @@
+// script.js
+
+document.getElementById("generateButton").addEventListener("click", generateBarcodes);
+
 function generateBarcodes() {
   var inputDigits = document.getElementById("inputDigits").value;
   var inputQuantity = document.getElementById("inputQuantity").value;
-  var barcodeSvg = document.getElementById("barcodeSvg");
+  var barcodeOutput = document.getElementById("barcodeOutput");
 
   // Limpar códigos anteriores
-  barcodeSvg.innerHTML = "";
+  barcodeOutput.innerHTML = "";
 
   // Remover caracteres não numéricos e garantir que o comprimento seja de 5 dígitos
   inputDigits = inputDigits.replace(/\D/g, ''); // Remove caracteres não numéricos
@@ -13,25 +17,15 @@ function generateBarcodes() {
     return;
   }
 
-  // Adicionando console.log para verificar se JsBarcode está definida
-  console.log("JsBarcode:", JsBarcode);
+  for (var i = 0; i < inputQuantity; i++) {
+    try {
+      var barcodeValue = generateEAN13(inputDigits, i);
 
-  // Certifique-se de que a variável JsBarcode seja uma função antes de chamá-la
-  if (typeof JsBarcode === 'function') {
-    for (var i = 0; i < inputQuantity; i++) {
-      var baseCode = "789" + inputDigits;
-      var fullCode = baseCode + (i + 1).toString().padStart(4, '0');
-      var checkDigit = calculateEAN13CheckDigit(fullCode);
-      var barcodeValue = fullCode + checkDigit;
-
-      JsBarcode(barcodeSvg, barcodeValue, {
-        format: "EAN13",
-        displayValue: true
-      });
-
-      barcodeSvg.innerHTML += "<br>";
+      // Adicione os códigos gerados ao elemento de saída
+      barcodeOutput.innerHTML += barcodeValue + "<br>";
+    } catch (error) {
+      alert(error.message);
+      return;
     }
-  } else {
-    alert("Erro: A biblioteca JsBarcode não está carregada corretamente.");
   }
 }
