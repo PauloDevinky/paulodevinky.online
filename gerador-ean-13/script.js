f2748d72621598baca9a512eca8e87e0ba6fc5a6
Paulo Devinky <120813278+PauloDevinky@users.noreply.github.com>
2023-11-14 12:34:09 -0300

    Update script.js

--- gerador-ean-13/script.js
+++ gerador-ean-13/script.js
@@ -33,3 +33,36 @@ function generateBarcodes() {
   // Atualizar o último número gerado no localStorage
   localStorage.setItem("lastGeneratedNumber", lastGeneratedNumber + inputQuantity);
 }
+
+// Função para calcular o dígito verificador do EAN-13
+function calculateEAN13CheckDigit(code) {
+  if (code.length !== 12) {
+    throw new Error("O código deve ter exatamente 12 dígitos.");
+  }
+
+  var digits = code.split("").map(Number);
+
+  var oddSum = digits
+    .filter(function (_, index) {
+      return index % 2 === 0;
+    })
+    .reduce(function (sum, digit) {
+      return sum + digit;
+    }, 0);
+
+  oddSum *= 3;
+
+  var evenSum = digits
+    .filter(function (_, index) {
+      return index % 2 !== 0;
+    })
+    .reduce(function (sum, digit) {
+      return sum + digit;
+    }, 0);
+
+  var totalSum = oddSum + evenSum;
+  var nextMultipleOfTen = Math.ceil(totalSum / 10) * 10;
+  var checkDigit = nextMultipleOfTen - totalSum;
+
+  return checkDigit.toString();
+}
